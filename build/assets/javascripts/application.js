f4675482e9abc576c337f0ccaab20b2a293acfbe
Nick Beattie <njbeattie@gmail.com>
2015-04-01 00:05:22 -0400

    Changed title

---
+++ build/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+// javascript
+(function() {
+})();
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwbGljYXRpb24uanMiLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJhcHBsaWNhdGlvbi5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBqYXZhc2NyaXB0XG4oZnVuY3Rpb24oKSB7XG59KSgpO1xuIl0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
